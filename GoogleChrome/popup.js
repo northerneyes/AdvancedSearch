0c2fe3dd82000bef588956378a814fa811d2707f
northerneyes <gbuhanov@gmail.com>
2012-12-13 23:58:49 +0600

    Styling.

--- GoogleChrome/popup.js
+++ GoogleChrome/popup.js
@@ -1,23 +1,14 @@
-var SearchTabId = undefined;
-var CurrentTabId = undefined;
-
-
-chrome.tabs.onRemoved.addListener(function(SearchTabId, removeInfo) {
-	console.log("CurrentTabId tab:" + CurrentTabId);
-	chrome.tabs.update(CurrentTabId, {selected: true});
-});
-
 $(function(){
-
+	 $('#query').focus();  
 	$('#query').keyup(function(e)
 	{
 		if (e.keyCode == 13) {
 			query = $(this).val();
 			console.log("You enter: " + query);
-			
-			var onSiteSearch = true;
-			var fileSearch = true;
-			var exactlyPhrase = true;
+
+			var onSiteSearch = $('#toogle-search-on_site').is(':checked');
+			var fileSearch = $('#toggle_file_search').is(':checked');
+			var exactlyPhrase = $('#toggle_search_exatly_phrase').is(':checked');
 			var filetype = "pdf";
 
 			chrome.tabs.getSelected(null, function(tab){
@@ -34,52 +25,9 @@ $(function(){
 			  			fileSearch: fileSearch,
 			  			exactlyPhrase: exactlyPhrase}
 			  	}); //send request to background.js
-			    //searchOnSite(tab.url, query);
 			})
 			
 		};
 	});
 
 });
-
-// function searchOnSite(url, query)
-// {
-// 	chrome.tabs.create({url: getSearchGoogleQueryForSite(query,url)}, function(tab){
-// 		chrome.extension.sendMessage({messageId: 1, currentTabId: tab.id}); //send request to background.js
-// 		//chrome.tabs.executeScript(tab.id, {file:"search.js"});
-// 		//SearchTabId = tab.id;
-// 	});
-// }
-
-// var req = new XMLHttpRequest();
-// req.open(
-// 	"GET",
-// 	"http://api.flickr.com/services/rest/?" +
-//         "method=flickr.photos.search&" +
-//         "api_key=90485e931f687a9b9c2a66bf58a3861a&" +
-//         "text=hello%20world&" +
-//         "safe_search=1&" +  // 1 is "safe"
-//         "content_type=1&" +  // 1 is "photos only"
-//         "sort=relevance&" +  // another good one is "interestingness-desc"
-//         "per_page=20",
-//     true);
-// req.onload = showPhotos;
-// req.send(null);
-
-// function showPhotos() {
-// 	var photos = req.responseXML.getElementsByTagName("photo");
-
-// 	for (var i = 0, photo; photo = photos[i]; i++) {
-// 	    var img = document.createElement("image");
-// 	    img.src = constructImageURL(photo);
-// 	    document.body.appendChild(img);
-// 	  }
-// }
-
-// function constructImageURL(photo) {
-//   return "http://farm" + photo.getAttribute("farm") +
-//       ".static.flickr.com/" + photo.getAttribute("server") +
-//       "/" + photo.getAttribute("id") +
-//       "_" + photo.getAttribute("secret") +
-//       "_s.jpg";
-// }
